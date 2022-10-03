f426e07fbaf8612d27b963a86b995983b31bd711
Aizendaisho <aizendaisho@gmail.com>
2022-10-03 16:55:56 -0400

    Completo

--- index.js
+++ index.js
@@ -3,32 +3,19 @@ const selecionados = document.querySelector('.selecionados')
 const contenedor1 = document.querySelector('.cont-1')
 const contenedor2 = document.querySelector('.cont-2')
 const num = document.querySelectorAll('.num')
-
+let rest = 0
 function escuchar (){
-    for (let i = 0; i < num.length; i++) {
-    num[i].addEventListener('click',(e)=>{
-        let nodo = e.target
-     if(nodo.classList.contains("active")){
-         nodo.classList.remove("active")
-        }else{        
-            nodo.classList.add("active")          
-        }
-    })    
-    }
-}
-
-const cuantos = ()=>{
-    let resultado = 0
-    num.forEach(nod=>{       
-        if(nod.classList.contains("active")) resultado++
+    num.forEach((nod,i,nodos)=>{
+        num[i].addEventListener('click',(e)=>{
+            nodos.forEach(ele=>ele.classList.remove("active"))
+            nod.classList.add("active")
+            rest = nod.textContent
+        })  
     })
-    return resultado
 }
-
 boton.addEventListener('click', ()=>{
-    selecionados.textContent = `You selected ${cuantos()} out of 5`
+    selecionados.textContent = `You selected ${rest} out of 5`
     contenedor1.classList.add("hide")
     contenedor2.classList.remove("hide")
 })
 escuchar()
-
